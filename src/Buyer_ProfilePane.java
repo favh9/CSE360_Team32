680905aefeb11feff36df1ca4d5061212f4c2caa
Fausto Velazquez <alexvelah0109@gmail.com>
2024-11-12 15:07:07 -0700

    KAN-32 add pane

--- src/Buyer_ProfilePane.java
+++ src/Buyer_ProfilePane.java
@@ -136,7 +136,7 @@ public class Buyer_ProfilePane extends BorderPane {
 
         // these are by default what we use for the scene
         this.setPrefSize(width, height);;
-        this.setBackground(Background.fill(Color.web("#4A1E2C"))); // darker maroon
+        this.setBackground(Background.fill(Color.web("#4A1E2C"))); // darker maroon color
     }
 
     public TextField getFirstnameTextField() {
