74671533fbfe98277f8a1cbec723cc04b0e95145
Fausto Velazquez <alexvelah0109@gmail.com>
2024-11-12 14:58:00 -0700

    KAN-43 add pane

--- src/NewUser_SelectUserTypePane.java
+++ src/NewUser_SelectUserTypePane.java
@@ -102,7 +102,7 @@ public class NewUser_SelectUserTypePane extends BorderPane {
 
         // these are by default what we use for the scene
         this.setPrefSize(width, height);;
-        this.setBackground(Background.fill(Color.web("#4A1E2C"))); // darker maroon
+        this.setBackground(Background.fill(Color.web("#4A1E2C"))); // darker maroon color
 
     }
 
