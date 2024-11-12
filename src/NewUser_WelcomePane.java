7f6e7c42a2116838702894b461ecb8e5b0db0720
Fausto Velazquez <alexvelah0109@gmail.com>
2024-11-12 14:50:47 -0700

    KAN-42 add pane

--- src/NewUser_WelcomePane.java
+++ src/NewUser_WelcomePane.java
@@ -78,7 +78,7 @@ public class NewUser_WelcomePane extends BorderPane {
 
         // these are by default what we use for the scene
         this.setPrefSize(width, height);;
-        this.setBackground(Background.fill(Color.web("#4A1E2C"))); // darker maroon
+        this.setBackground(Background.fill(Color.web("#4A1E2C"))); // darker maroon color
 
     }
 
