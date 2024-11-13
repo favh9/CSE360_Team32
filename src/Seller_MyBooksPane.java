5b048901b74c505e06e51b0aa707055235b106cd
Fausto Velazquez <alexvelah0109@gmail.com>
2024-11-13 12:56:45 -0700

    KAN-29 add list/scrollpane

--- src/Seller_MyBooksPane.java
+++ src/Seller_MyBooksPane.java
@@ -1,14 +1,18 @@
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 
 public class Seller_MyBooksPane extends BorderPane {
+
+    private VBox listVBox;
 
     public Seller_MyBooksPane(User user, double width, double height) {
 
@@ -22,8 +26,52 @@ public class Seller_MyBooksPane extends BorderPane {
         HBox headerHBox = new HBox(titleLabel);
         headerHBox.setAlignment(Pos.BASELINE_LEFT);
 
+        // CHANGE
+        // set attributes for the header of the body
+        // set attributes for the header label
+        Label headerLabel1 = new Label("Title");
+        headerLabel1.setFont(Font.font(20));
+        headerLabel1.setPrefWidth(390);
+        headerLabel1.setAlignment(Pos.BASELINE_LEFT);
+
+        // set attributes for the header label
+        Label headerLabel2 = new Label("Price");
+        headerLabel2.setFont(Font.font(20));
+        headerLabel2.setPrefWidth(150);
+        headerLabel2.setAlignment(Pos.BASELINE_CENTER);
+
+        // set attributes for the header label
+        Label headerLabel3 = new Label("Amount");
+        headerLabel3.setFont(Font.font(20));
+        headerLabel3.setPrefWidth(150);
+        headerLabel3.setAlignment(Pos.BASELINE_CENTER);
+
+        // set attributes for the header of the body
+        HBox bodyheaderHBox = new HBox(headerLabel1,headerLabel2,headerLabel3);
+
+        // set attributes for the line that separates the header and the scrollpane
+        Rectangle lineSeparator = new Rectangle();
+        lineSeparator.setWidth(695);
+        lineSeparator.setHeight(2);
+        lineSeparator.setFill(Color.BLACK);
+
+        // set attributes for the VBox that holds the lists of users
+        listVBox = new VBox();
+
+        // set attributes for the scrollpane
+        ScrollPane sp = new ScrollPane(listVBox);
+        sp.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        sp.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        sp.minViewportHeightProperty().set(400);
+        sp.setPrefHeight(400);
+        sp.setStyle("-fx-background: white;");
+
+        // set attributes for the users box that includes their header and scrollpane
+        VBox headerAndScrollPane = new VBox(bodyheaderHBox,lineSeparator,sp);
+        // CHANGE
+
         // set attributes for the main VBox
-        VBox mainVBox = new VBox(headerHBox);
+        VBox mainVBox = new VBox(headerHBox,headerAndScrollPane);
         mainVBox.setPadding(new Insets(40,40,0,40));
         mainVBox.setPrefWidth(width - navBarVBox.getWidth() - 40);
         mainVBox.setStyle(
