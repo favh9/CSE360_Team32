c989d96e7c3b74035dfebc30897b93b7e722dd25
Fausto Velazquez <alexvelah0109@gmail.com>
2024-11-20 22:10:32 -0700

    --test

--- src/Buyer_OrdersPane.java
+++ src/Buyer_OrdersPane.java
@@ -1,13 +1,11 @@
+import java.text.NumberFormat;
+
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
@@ -15,25 +13,22 @@ import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 
-import java.text.NumberFormat;
-
-
 public class Buyer_OrdersPane extends BorderPane {
 
-    private final Button b1;
-    private final Button b2;
-    private final VBox listVBox;
-    private final Text quantityText;
+    private Button refreshButton;
+    private VBox transactionsVBox;
+    private Text transactionsAmountText;
 
     public Buyer_OrdersPane(User user, double width, double height) {
 
         Font titleFont = Font.font("Arial", FontWeight.NORMAL, FontPosture.REGULAR, 42);
         Font quantityFont = Font.font("Arial", FontWeight.NORMAL, FontPosture.REGULAR, 24);
 
-        Buyer_NavigationControl navBarVBox = new Buyer_NavigationControl(user,width,height);
+        Buyer_NavigationControl navigationControl = new Buyer_NavigationControl(user, width, height);
+
 
         // set attributes for the Title Label
-        Label titleLabel = new Label("Orders");
+        Label titleLabel = new Label("Transactions");
         titleLabel.setFont(titleFont);
 
         // set attributes for the quantity label
@@ -41,11 +36,11 @@ public class Buyer_OrdersPane extends BorderPane {
         quantityLabel.setFont(quantityFont);
 
         // set attribute for the quantity label
-        quantityText = new Text("0");
-        quantityText.setFont(quantityFont);
+        transactionsAmountText = new Text("0");
+        transactionsAmountText.setFont(quantityFont);
 
         // set attributes for the HBox that holds the quantity and quantity label
-        HBox quantityHBox = new HBox(quantityText, quantityLabel);
+        HBox quantityHBox = new HBox(transactionsAmountText, quantityLabel);
         quantityHBox.setSpacing(20);
         quantityHBox.setAlignment(Pos.BASELINE_CENTER);
 
@@ -60,24 +55,13 @@ public class Buyer_OrdersPane extends BorderPane {
         image1.setFitHeight(25);
 
         // set attributes for the button
-        b1 = new Button();
-        b1.setGraphic(image1);
-        b1.setBackground(Background.fill(Color.TRANSPARENT));
-        b1.setAlignment(Pos.CENTER);
-
-        // set attributes for button image
-        ImageView image2 = new ImageView(Main.delUserIcon);
-        image2.setFitHeight(30);
-        image2.setFitWidth(30);
-
-        // set attributes for button
-        b2 = new Button();
-        b2.setGraphic(image2);
-        b2.setBackground(Background.fill(Color.TRANSPARENT));
-        b2.setAlignment(Pos.CENTER);
+        refreshButton = new Button();
+        refreshButton.setGraphic(image1);
+        refreshButton.setBackground(Background.fill(Color.TRANSPARENT));
+        refreshButton.setAlignment(Pos.CENTER);
 
         // set attributes for the container of two buttons
-        HBox rightHeaderPaneHBox = new HBox(b1,b2);
+        HBox rightHeaderPaneHBox = new HBox(refreshButton);
         rightHeaderPaneHBox.setAlignment(Pos.CENTER_RIGHT);
 
         // set attributes for the header of the page
@@ -85,39 +69,54 @@ public class Buyer_OrdersPane extends BorderPane {
         headerPane.setLeft(leftHeaderPaneHBox);
         headerPane.setRight(rightHeaderPaneHBox);
 
-        // set attributes for the header of the body
         // set attributes for the header label
-        Label headerLabel1 = new Label("Title");
+        Label headerLabel1 = new Label("Book Title");
+        headerLabel1.setPrefWidth(200);
+        headerLabel1.setWrapText(true);
         headerLabel1.setFont(Font.font(20));
-        headerLabel1.setPrefWidth(390);
-        headerLabel1.setAlignment(Pos.BASELINE_LEFT);
+        headerLabel1.setAlignment(Pos.BASELINE_CENTER);
 
         // set attributes for the header label
-        Label headerLabel2 = new Label("Ordered on");
+        Label headerLabel2 = new Label("Timestamp");
+        headerLabel2.setPrefWidth(200);
+        headerLabel2.setWrapText(false);
         headerLabel2.setFont(Font.font(20));
-        headerLabel2.setPrefWidth(150);
         headerLabel2.setAlignment(Pos.BASELINE_CENTER);
 
         // set attributes for the header label
-        Label headerLabel3 = new Label("Amount");
-        headerLabel3.setFont(Font.font(20));
+        Label headerLabel3 = new Label("Profit");
         headerLabel3.setPrefWidth(150);
+        headerLabel3.setWrapText(false);
+        headerLabel3.setFont(Font.font(20));
         headerLabel3.setAlignment(Pos.BASELINE_CENTER);
 
+        // set attributes for the header label
+        Label headerLabel4 = new Label("Sold To");
+        headerLabel4.setPrefWidth(150);
+        headerLabel4.setWrapText(true);
+        headerLabel4.setFont(Font.font(20));
+        headerLabel4.setAlignment(Pos.BASELINE_CENTER);
+
+        // FOR DEBUGGING maybe?
+        headerLabel1.setBorder(Border.stroke(Color.BLACK));
+        headerLabel2.setBorder(Border.stroke(Color.BLACK));
+        headerLabel3.setBorder(Border.stroke(Color.BLACK));
+        headerLabel4.setBorder(Border.stroke(Color.BLACK));
+
         // set attributes for the header of the body
-        HBox bodyheaderHBox = new HBox(headerLabel1,headerLabel2,headerLabel3);
+        HBox bodyheaderPane = new HBox(headerLabel1,headerLabel2,headerLabel3,headerLabel4);
 
         // set attributes for the line that separates the header and the scrollpane
         Rectangle lineSeparator = new Rectangle();
-        lineSeparator.setWidth(690);
+        lineSeparator.setWidth(700);
         lineSeparator.setHeight(2);
         lineSeparator.setFill(Color.BLACK);
 
         // set attributes for the VBox that holds the lists of users
-        listVBox = new VBox();
+        transactionsVBox = new VBox();
 
         // set attributes for the scrollpane
-        ScrollPane sp = new ScrollPane(listVBox);
+        ScrollPane sp = new ScrollPane(transactionsVBox);
         sp.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         sp.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         sp.minViewportHeightProperty().set(400);
@@ -125,19 +124,19 @@ public class Buyer_OrdersPane extends BorderPane {
         sp.setStyle("-fx-background: white;");
 
         // set attributes for the users box that includes their header and scrollpane
-        VBox headerAndScrollPane = new VBox(bodyheaderHBox,lineSeparator,sp);
+        VBox headerAndScrollPane = new VBox(bodyheaderPane,lineSeparator,sp);
 
         // set attributes for the main VBox which excludes the navigation bar
         VBox mainVBox = new VBox(headerPane,headerAndScrollPane);
         mainVBox.setPadding(new Insets(40,40,0,40));
         mainVBox.setSpacing(10);
-        mainVBox.setPrefWidth(width - navBarVBox.getWidth() - 40);
+        mainVBox.setPrefWidth(width - navigationControl.getWidth() - 40);
         mainVBox.setStyle(
                 "-fx-background-radius: 2em;" + "-fx-background-color: #ffffff;"
         );
 
         // set attributes for the container that holds the navigation bar and page
-        HBox navBarAndMainHBox = new HBox(navBarVBox,mainVBox);
+        HBox navBarAndMainHBox = new HBox(navigationControl,mainVBox);
         navBarAndMainHBox.setSpacing(20);
 
         this.setCenter(navBarAndMainHBox);
@@ -149,13 +148,9 @@ public class Buyer_OrdersPane extends BorderPane {
 
     }
 
-    public Button getButton1() {
-        return b1;
-    }
-
     public void clearTransactions() {
-        listVBox.getChildren().clear();
-        quantityText.setText("0");
+        transactionsVBox.getChildren().clear();
+        transactionsAmountText.setText("0");
     }
 
     // returns a container where a user's information is displayed
@@ -200,16 +195,113 @@ public class Buyer_OrdersPane extends BorderPane {
 
         VBox vbox = new VBox(hbox,lineSeparator);
 
-        listVBox.getChildren().add(vbox);
+        transactionsVBox.getChildren().add(vbox);
         this.updateCount();
     }
 
-    // modifies and updates the display amount text
+    // modifies and updates the display amount text by one
     public void updateCount() {
-        String str = quantityText.getText();
+        String str = transactionsAmountText.getText();
         int incr = Integer.parseInt(str) + 1;
-        quantityText.setText(Integer.toString(incr));
+        transactionsAmountText.setText(Integer.toString(incr));
     }
 
-}
+    // modifies and updates the display amount text by an amount
+    public void updateCount(int num) {
+        String str = transactionsAmountText.getText();
+        int incr = Integer.parseInt(str) + num;
+        transactionsAmountText.setText(Integer.toString(incr));
+    }
 
+    public Button getRefreshButton() {
+        return refreshButton;
+    }
+
+    public void addAllTransactionsToPane() {
+
+        int testAmountOfBooks = 20;
+
+        for(int i = 0; i < testAmountOfBooks; i++){
+            addTransactionToPane(new Transaction());
+            updateCount();
+        }
+
+    }
+
+    public void addTransactionToPane(Transaction transaction) {
+
+        // set attributes for the header label
+        Label title = new Label("Theory to the universe of things in my world");
+        title.setPrefWidth(200);
+        title.setWrapText(true);
+        title.setFont(Font.font(20));
+        title.setAlignment(Pos.BASELINE_LEFT);
+        title.setPadding(new Insets(5));
+
+        VBox titleBox = new VBox(title);
+        titleBox.setAlignment(Pos.CENTER);
+
+        // set attributes for the header label
+        Label timestamp = new Label(transaction.getTimestamp());
+        timestamp.setPrefWidth(200);
+        timestamp.setWrapText(false);
+        timestamp.setFont(Font.font(20));
+        timestamp.setAlignment(Pos.BASELINE_CENTER);
+        timestamp.setPadding(new Insets(5));
+
+        VBox timestampBox = new VBox(timestamp);
+        timestampBox.setAlignment(Pos.CENTER);
+
+        // set attributes for the header label
+        Label amount = new Label("$" + transaction.getAmount());
+        amount.setPrefWidth(150);
+        amount.setWrapText(false);
+        amount.setFont(Font.font(20));
+        amount.setAlignment(Pos.BASELINE_CENTER);
+        amount.setPadding(new Insets(5));
+
+        VBox amountBox = new VBox(amount);
+        amountBox.setAlignment(Pos.CENTER);
+
+        Hyperlink soldtoLink = new Hyperlink();
+        soldtoLink.setText("The Amazing Gatsby");
+        soldtoLink.setWrapText(true);
+        soldtoLink.setPrefWidth(150);
+        soldtoLink.setFont(Font.font(20));
+        soldtoLink.setAlignment(Pos.BASELINE_CENTER);
+        soldtoLink.setPadding(new Insets(5));
+
+        soldtoLink.setOnAction(e->{
+
+            Alert review = new Alert(Alert.AlertType.INFORMATION);
+            review.setTitle(null);
+            review.setHeaderText(null);
+            review.setContentText("This is the buyer's review");
+            review.show();
+
+        });
+
+        // set attributes for the header label
+        Label soldto = new Label("SparkyandHisFriends");
+        soldto.setPrefWidth(150);
+        soldto.setWrapText(true);
+        soldto.setFont(Font.font(20));
+        soldto.setAlignment(Pos.BASELINE_CENTER);
+        soldto.setPadding(new Insets(5));
+
+        VBox soldToBox = new VBox(soldtoLink);
+        soldToBox.setAlignment(Pos.CENTER);
+
+        HBox transactionHBox = new HBox(titleBox,timestampBox,amountBox,soldToBox);
+
+        // set attributes for the line that separates the header and the scrollpane
+        Rectangle lineSeparator = new Rectangle();
+        lineSeparator.setWidth(700);
+        lineSeparator.setHeight(1);
+        lineSeparator.setFill(Color.BLACK);
+
+        transactionsVBox.getChildren().add(transactionHBox);
+        transactionsVBox.getChildren().add(lineSeparator);
+
+    }
+}
